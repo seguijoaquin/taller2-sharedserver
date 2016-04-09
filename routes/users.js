d8c1f7c060ae54ee2a8bc8194f18057c7e5b4dd8
seguijoaquin <segui.joaquin@gmail.com>
2016-04-09 18:18:34 -0300

    Agrego Post

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  //Leer la base de datos y devolver todo
 });
 
+//Alta de usuario
+router.post('/',function(req, res, next) {
+  //Agrego el json a la base de datos
+  res.sendStatus(201);
+});
 module.exports = router;
