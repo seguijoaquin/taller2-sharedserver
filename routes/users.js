ed89831535fe83ee64b5a3685929778b8519fd9b
seguijoaquin <segui.joaquin@gmail.com>
2016-04-10 01:14:42 -0300

    Agrego alta y baja de usuarios

--- routes/users.js
+++ routes/users.js
@@ -12,11 +12,29 @@ router.get('/', function(req, res, next) {
 });
 
 //Alta de usuario
-//Recibe objeto json y agrega
+// El objeto json viene con id null, se le asigna id al hacer insert
+// obteniendo el nro con su primal key
 //Devuelve http 201
 router.post('/',function(req, res, next) {
+  //TODO: Necesito chequear que la tabla de usuarios esta creada
   //TODO: Necesito chequear si ya existe el usuario a traves del mail
-
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      done();
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    client.query("INSERT INTO users (data) values($1) RETURNING id",[req.body.user],function(err, result) {
+      done(); //Devuelvo el cliente al pool xq no necesito más la conexion
+      if (err) {
+        console.log(err);
+      } else {
+        req.body.user.id = result.rows[0].id;
+        console.log(req.body.user);
+        res.status(201).json(req.body.user);
+      }
+    });
+  });
 });
 
 //Consulta perfil de usuario
@@ -47,13 +65,24 @@ router.put('/[0-9]+/photo',function(req, res, next) {
 router.delete('/[0-9]+',function(req, res, next) {
   //TODO: Chequear si existe el usuario
 
-});
-
-
-router.post('/profile', function (req, res, next) {
-  res.send('profile');
-  console.log(req.body);
-  res.json(req.body);
+  //Obtengo usr ID desde url
+  var usrID = req.url.substring(1);
+  // Get a Postgres client from the connection pool
+  pg.connect(urlDB, function(err, client, done) {
+    if(err) {
+      done(); //Devuelvo el cliente al pool
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    client.query("DELETE FROM users WHERE id=($1)", [id],function(err, result) {
+      done(); //Call done() to get the client back to the pool
+      if (err) {
+        console.log(err);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  });
 });
 
 module.exports = router;
