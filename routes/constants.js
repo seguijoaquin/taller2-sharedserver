f7c8aaa3c74501920c1ddfa3732133988eacc41b
diegofk26 <diegofk26@gmail.com>
2016-04-24 20:14:59 -0300

    Agrego la validacion de la existencia de la tabla mas un poco de refactor en el manejo de error de la conexion con pg

---
+++ routes/constants.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+//----------------------------- ERRORES ---------------------------------------
+
+const ERROR_MSG_PG_CONNECT = "[ERROR: No se pudo realizar la conexion a la base de datos]";
+
+const ERROR_MSG_DB_TABLA_INEXISTENTE = "[ERROR: La tabla consultada es inexistente.]";
+
+const CODE_TABLA_INEXISTENTE = '42P01';
--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,46 @@ var pg = require('pg');
 var urlDB = "postgres://tqezweoinbznuw:8DX2r1Jt6SuzmPlqyRoEUwSQKr@ec2-54-221-249-201.compute-1.amazonaws.com:5432/d1h0hefo2t4jcr"
 pg.defaults.ssl = true;
 
+
+
+function handleQueryError(err,res){
+  //La tabla no existe.
+  if (err.code == '42P01'){
+    console.log("[ERROR: La tabla consultada es inexistente.]");
+    res.status(400);
+    res.end();
+  }
+
+
+};
+
+
+function handlePgConnectError(err,res,done){
+    done(); //Devuelvo el cliente al pool
+    console.log("[ERROR: No se pudo realizar la conexion a la base de datos]");
+    console.log(err);
+    return res.sendStatus(500);
+}
+
+
+
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // Get a Postgres client from the connection pool
   pg.connect(urlDB, function(err, client, done) {
-    if(err) {
-      done(); //Devuelvo el cliente al pool
-      console.log(err);
-      return res.sendStatus(500);
+
+    if(err){
+      return handlePgConnectError(err,res,done);
     }
+
     var query = client.query("SELECT * FROM usuarios",function(err, result) {
       done(); //Devuelvo el cliente al pool xq no necesito más la conexion
       if (err) {
-        console.log(err);
+        handleQueryError(err,res);
       } else {
         query.on('row', function(row,result) {
           //fired once for each row returned
@@ -57,17 +84,19 @@ router.post('/',function(req, res, next) {
   //TODO: Necesito chequear que la tabla de usuarios esta creada
   //TODO: Necesito chequear si ya existe el usuario a traves del mail
   pg.connect(urlDB, function(err, client, done) {
-    if(err) {
-      done();
-      console.log(err);
-      return res.sendStatus(500);
+    //Si no se pudo conectar a la base de datos manda error
+    if(err){
+      return handlePgConnectError(err,res,done);
     }
+
     //client.query("CREATE TABLE IF NOT EXISTS usuarios (id SERIAL PRIMARY KEY, name VARCHAR(40), mail VARCHAR(40), alias VARCHAR(40), latitude REAL, longitude REAL)");
     client.query("INSERT INTO usuarios (name,mail,alias,latitude,longitude) values($1,$2,$3,$4,$5) RETURNING id",
     [req.body.user.name,req.body.user.email,req.body.user.alias,req.body.user.location.latitude,req.body.user.location.longitude],function(err, result) {
       done(); //Devuelvo el cliente al pool xq no necesito más la conexion
+
       if (err) {
-        console.log(err);
+        handleQueryError(err,res);
+
       } else {
         var jsonObject = { user : req.body.user, metadata : 1.0}
         res.status(201).json(jsonObject);
@@ -85,15 +114,15 @@ router.get('/[0-9]+',function(req, res, next) {
   var usrID = req.url.substring(1); //Substring después de la primer '/'
   // Get a Postgres client from the connection pool
   pg.connect(urlDB, function(err, client, done) {
-    if(err) {
-      done(); //Devuelvo el cliente al pool
-      console.log(err);
-      return res.sendStatus(500);
+
+    if(err){
+      return handlePgConectError(err,res,done);
     }
+
     client.query("SELECT * FROM usuarios WHERE id = ($1)",[usrID],function(err, result) {
       done(); //Devuelvo el cliente al pool xq no necesito más la conexion
       if (err) {
-        console.log(err);
+        handleQueryError(err,res);
       } else {
         //Chequeo que la query devuelva un usuario
         //En caso de que haya varios, devuelve el primero
@@ -130,16 +159,16 @@ router.put('/[0-9]+',function(req, res, next) {
   // Get a Postgres client from the connection pool
   if (req.body.user.id == usrID) {
     pg.connect(urlDB, function(err, client, done) {
-      if(err) {
-        done(); //Devuelvo el cliente al pool
-        console.log(err);
-        return res.sendStatus(500);
+
+      if(err){
+        return handlePgConnectError(err,res,done);
       }
+
       client.query("UPDATE usuarios SET name = ($1), mail = ($2), alias = ($3), latitude = ($4), longitude = ($5) WHERE id = ($6)",
       [req.body.user.name, req.body.user.email, req.body.user.alias, req.body.user.location.latitude, req.body.user.location.longitude, usrID],function(err, result) {
         done(); //Devuelvo el cliente al pool xq no necesito más la conexion
         if (err) {
-          console.log(err);
+          handleQueryError(err,res);
         } else {
           res.sendStatus(200);
         }
@@ -168,17 +197,16 @@ router.delete('/[0-9]+',function(req, res, next) {
   var usrID = req.url.substring(1); //Substring después de la primer '/'
   // Get a Postgres client from the connection pool
   pg.connect(urlDB, function(err, client, done) {
-    if(err) {
-      done(); //Devuelvo el cliente al pool
-      console.log(err);
-      return res.sendStatus(500);
+    if(err){
+      return handlePgConectError(err,res,done);
     }
+
     client.query("DELETE FROM usuarios WHERE id = ($1)", [usrID],function(err, result) {
       done(); //Call done() to get the client back to the pool
       if (err) {
-        console.log(err);
+        handleQueryError(err,res);
       } else {
-        
+
         // TODO: verificar si se elimino el usuario o no.
 
         res.sendStatus(200);
