3d2ffb02e33b733c549665164086c471bfe05298
seguijoaquin <segui.joaquin@gmail.com>
2016-04-28 04:12:40 -0300

    Cambio el orden de llamada de conexion ssl a pg

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+pg.defaults.ssl = true;
+
 var Constants = require('./constants.js');
 
 //var urlDB = process.env.DATABASE_URL;
 var urlDB = Constants.POSTGRE_URL_DB;
 
-
-
 function query(text, values, cb) {
-
   //Me conecto a la base de datos postgre esto se repite en todos
   pg.connect( urlDB , function(err, client, done) {
     if(err){
@@ -29,10 +28,6 @@ function query(text, values, cb) {
   });
 }
 
-pg.defaults.ssl = true;
-
-
-
 function handleQueryError(err,res){
   //La tabla no existe.
   if (err.code == Constants.CODE_TABLA_INEXISTENTE){
@@ -40,7 +35,6 @@ function handleQueryError(err,res){
     res.status(400);
     res.end();
   }
-
 };
 
 
@@ -51,23 +45,12 @@ function handlePgConnectError(err,res,done){
   return res.sendStatus(500);
 };
 
-
-
-
-
-
-
-
 //Listado de usuarios
 /* GET users listing. */
 router.get(Constants.DIR_LISTADO_USUARIOS, function(req, res, next) {
   // Get a Postgres client from the connection pool
-
-
-  query(Constants.UERY_LISTADO_USUARIOS,null,
+  query(Constants.QUERY_LISTADO_USUARIOS,null,
     function(err, result) {
-
-
       query.on('end', function(result) {
         //fired once and only once, after the last row has been returned and after all 'row' events are emitted
         //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
@@ -88,17 +71,9 @@ router.get(Constants.DIR_LISTADO_USUARIOS, function(req, res, next) {
         }
         return res.json(jsonObject);
       })
-
-
     }
   );
-
-
 });
-
-
-
-
 
 //Alta de usuario
 // El objeto json viene con id null, se le asigna id al hacer insert
@@ -108,22 +83,14 @@ router.post(Constants.DIR_ALTA_USUARIOS,function(req, res, next) {
   //TODO: Necesito chequear que la tabla de usuarios esta creada
   //TODO: Necesito chequear si ya existe el usuario a traves del mail
 
-  query(Constants.QUERY_ALTA_USUARIO,
-    [req.body.user.name,req.body.user.email,req.body.user.alias,req.body.user.location.latitude,
-      req.body.user.location.longitude],
-      function(err,result){
-
-        var jsonObject = { user : req.body.user, metadata : 1.0};
-        res.status(201).json(jsonObject);
-        res.end();
-
+  query(Constants.QUERY_ALTA_USUARIO, [req.body.user.name,req.body.user.email,req.body.user.alias,req.body.user.location.latitude, req.body.user.location.longitude],
+    function(err,result){
+      var jsonObject = { user : req.body.user, metadata : 1.0};
+      res.status(201).json(jsonObject);
+      res.end();
       }
-    );
-
+  );
 });
-
-
-
 
 //Consulta perfil de usuario
 //Recibe el id del usuario por url, parsea y busca
@@ -131,7 +98,6 @@ router.post(Constants.DIR_ALTA_USUARIOS,function(req, res, next) {
 router.get(Constants.DIR_CONSULTA_PERFIL_USUARIO,function(req, res, next) {
   //Obtengo usr ID desde url
   var usrID = req.url.substring(1); //Substring después de la primer '/'
-
 
   query(Constants.QUERY_CONSULTA_PERFIL_USUARIO,[usrID],
     function(err,result){
@@ -156,46 +122,29 @@ router.get(Constants.DIR_CONSULTA_PERFIL_USUARIO,function(req, res, next) {
         return res.json(jsonObject);
       } else {
         res.sendStatus(404); //User not found
-
       }
-    });
-
-  });
-
-
-
-
+    }
+  );
+});
 
 //Modifica el perfil de un usuario
 //Recibe un json con un usuario
 router.put(Constants.DIR_MODIFICACION_PERFIL_USUARIOS,function(req, res, next) {
-//Obtengo usr ID desde url
-var usrID = req.url.substring(1); //Substring después de la primer '/'
-// Get a Postgres client from the connection pool
-if (req.body.user.id == usrID) {
+  //Obtengo usr ID desde url
+  var usrID = req.url.substring(1); //Substring después de la primer '/'
+  // Get a Postgres client from the connection pool
+  if (req.body.user.id == usrID) {
+    query(Constants.QUERY_MODIFICACION_PERFIL_USUARIO,
+      [req.body.user.name, req.body.user.email, req.body.user.alias,
+        req.body.user.location.latitude, req.body.user.location.longitude, usrID],
+        function(err, result) {
+          res.sendStatus(200);
+        });
+  } else {
+    res.sendStatus(418); // TODO: NO COINCIDE EL ID
+  }
+});
 
-
-  query(Constants.QUERY_MODIFICACION_PERFIL_USUARIO,
-    [req.body.user.name, req.body.user.email, req.body.user.alias,
-      req.body.user.location.latitude, req.body.user.location.longitude, usrID],
-      function(err, result) {
-        res.sendStatus(200);
-      });
-
-    } else {
-      res.sendStatus(418); // TODO: NO COINCIDE EL ID
-    }
-  });
-
-
-
-
-  /*Actualiza foto de perfil
-  Parametros recibidos:
-  {
-  "photo": “base_64”
-}
-*/
 router.put(Constants.DIR_ACTUALIZA_FOTO_PERFIL,function(req, res, next) {
   res.sendStatus(200);
 });
@@ -207,17 +156,11 @@ router.delete(Constants.DIR_BAJA_DE_USUARIOS,function(req, res, next) {
   //Obtengo usr ID desde url
   var usrID = req.url.substring(1); //Substring después de la primer '/'
   // Get a Postgres client from the connection pool
-
-
   query(Constants.QUERY_BAJA_DE_USUARIO, [usrID],
     function(err, result) {
-
       // TODO: verificar si se elimino el usuario o no.
       res.sendStatus(200);
-
     });
-
-
-});
+  });
 
 module.exports = router;
