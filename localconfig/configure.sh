faba6dcdb89105c458fb8f1d48061908e0eec3a1
diegofk26 <diegofk26@gmail.com>
2016-05-04 17:26:32 -0300

    Scripts para configuracion local

---
+++ localconfig/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo /etc/init.d/postgresql start
+
+
+su postgres  ./postgreconfig.sh
+
+
+#CAMBIO LA URL DE LA DB a local
+sed -i.bak "s-POSTGRE_URL_DB:.*-POSTGRE_URL_DB: \"postgres://localhost:5432/myDB\",-g" constants.js
+
+#Cambiar SSL en users.js
+cd ../routes/
+sed -i.bak "s-pg\.defaults\.ssl =.*-//pg\.defaults\.ssl = true;-g" users.js
---
+++ localconfig/postgreconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+psql myDB -c "CREATE TABLE IF NOT EXISTS usuarios (
+id SERIAL PRIMARY KEY,
+name VARCHAR(40),
+mail VARCHAR(40),
+alias VARCHAR(40),
+latitude REAL,
+longitude REAL);"
