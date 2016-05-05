7397c3ff0bf9b57337468fa1569dda2c44529da3
diegofk26 <diegofk26@gmail.com>
2016-05-05 00:29:47 -0300

    arreglo en la configuracion local

--- localconfig/configure.sh
+++ localconfig/configure.sh
@@ -5,9 +5,12 @@ sudo /etc/init.d/postgresql start
 
 su postgres  ./postgreconfig.sh
 
+cd ..
 
 #CAMBIO LA URL DE LA DB a local
 sed -i.bak "s-POSTGRE_URL_DB:.*-POSTGRE_URL_DB: \"postgres://localhost:5432/myDB\",-g" constants.js
+
+cd routes
 
 #Cambiar SSL en users.js
 cd ../routes/
