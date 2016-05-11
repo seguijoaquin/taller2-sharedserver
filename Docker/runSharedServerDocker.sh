eb16e66b589e7387f8c6d803280a349cd18fc69e
diegofk26 <diegofk26@gmail.com>
2016-05-11 16:51:26 -0300

    Borro el container si ya estaba siendo ejecutado o ya existe

--- Docker/runSharedServerDocker.sh
+++ Docker/runSharedServerDocker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker rm -f sharedlocal
+
 
 docker run -i -w /home/taller2-sharedserver/localconfig -p 5000:5000 -d --name sharedlocal diegofk26/shared-server-docker sh ./runlocally.sh
 
