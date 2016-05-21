2fbd515af2db1be577472a9ef1715e094b989339
seguijoaquin <segui.joaquin@gmail.com>
2016-05-21 18:28:44 -0300

    Fixes #21 - Correcion de errores en Armar JSON lista de intereses

--- handlers/json_handler.js
+++ handlers/json_handler.js
@@ -42,8 +42,8 @@ json_handler.armarJsonListaIntereses = function(result) {
   var jsonObject = { "interests" : [] , metadata : { version : Constants.METADATA_VERSION , count : result.rowCount}}
   for (var i = 0; i < result.rowCount; i++) {
     var oneInterest = {
-      category : result.interest.category,
-      value : result.interest.value
+      category : result.rows[i].interest.category,
+      value : result.rows[i].interest.value
     }
     jsonObject.interests.push(oneInterest);
   }
