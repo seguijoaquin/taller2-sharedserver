3138a5fbf32fe70c53f8f9d167d7c21e278c593a
seguijoaquin <segui.joaquin@gmail.com>
2016-06-13 01:42:11 -0300

    Closes #16 - Completo plantillas de html de todas las paginas necesarias, con bootstrap incluido

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Shared server Interface' });
 });
 
+router.get('/test_page', function(req, res, next) {
+  res.render('test_page', { title: 'Shared server Interface' });
+});
+
 module.exports = router;
