49c72848cd9b6c45f324decbf430170a6a275754
seguijoaquin <segui.joaquin@gmail.com>
2016-06-13 17:19:28 -0300

    Agrego index.html a las vistas y modifico la forma de rutear los archivos html desde index.js - #33

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shared server Interface' });
+  //res.render('index', { title: 'Shared server Interface' });
+  //__dirname contiene la ruta del archivo index.js
+  //Y necesito la ruta de los archivos html que quiero mandar al browser
+  res.sendFile(path.join(__dirname,"../public",index.html));
 });
 
 router.get('/test_page', function(req, res, next) {
   res.render('test_page', { title: 'Shared server Interface' });
 });
+
+//Captura las rutas de las paginas html en public
+router.get('/*.html', function(req, res) {
+  res.sendFile(path.join(__dirname,"../public",req.html));
+});
+
 
 module.exports = router;
