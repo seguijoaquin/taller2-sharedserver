3c21baa130718eefafd4d4a8abd57d71146ebd9c
seguijoaquin <segui.joaquin@gmail.com>
2016-06-15 13:26:26 -0300

    Closes #40 - Funcionalidad para buscar un usuario por su id

--- public/js/my_controller.js
+++ public/js/my_controller.js
@@ -2,13 +2,23 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope,$http) {
 
+  $scope.user = {}
   $scope.lista_usuarios = [];
+  $scope.id_user = null;
   $scope.showMe = false;
 
   $scope.getUsers = function () {
     $http.get("/users").then(function(response) {
       console.log(response.data);
       $scope.lista_usuarios = response.data.users;
+    });
+  };
+
+  $scope.getUser = function () {
+    $http.get("/users/"+$scope.id_user).then(function(response) {
+      console.log(response.data);
+      $scope.showMe = true;
+      $scope.user = [response.data.user];
     });
   };
 
