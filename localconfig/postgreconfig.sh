c638bb9fed956e21b6cae3079f441543ebd97fcb
diegofk26 <diegofk26@gmail.com>
2016-06-17 12:57:49 -0300

    Actualizo las tablas manualmente para el server local

--- localconfig/postgreconfig.sh
+++ localconfig/postgreconfig.sh
@@ -2,10 +2,21 @@
 
 createdb myDB
 
-psql myDB -c "CREATE TABLE IF NOT EXISTS usuarios (
-id SERIAL PRIMARY KEY,
-name VARCHAR(40),
-mail VARCHAR(40),
-alias VARCHAR(40),
-latitude REAL,
-longitude REAL);"
+#Users
+psql myDB -c "CREATE TABLE users ( id_user SERIAL PRIMARY KEY, name TEXT NOT NULL, alias TEXT, email TEXT NOT NULL, sex TEXT NOT NULL, edad INTEGER NOT NULL, latitude REAL, longitude REAL, photo_profile TEXT );"
+
+
+#Pictures
+
+psql myDB -c "CREATE TABLE pictures ( id_picture SERIAL PRIMARY KEY, id_user INTEGER NOT NULL REFERENCES users(id_user) ON DELETE CASCADE, pic TEXT );"
+
+#Interests
+
+psql myDB -c "CREATE TABLE interests ( id_interest SERIAL PRIMARY KEY, category TEXT NOT NULL REFERENCES categories(category) ON DELETE CASCADE, value TEXT, UNIQUE (category, value) );"
+
+#User_Interests
+
+psql myDB -c "CREATE TABLE users_interests ( id_usr_int SERIAL PRIMARY KEY, id_user INTEGER NOT NULL REFERENCES users(id_user) ON DELETE CASCADE, id_interest INTEGER NOT NULL REFERENCES interests(id_interest) ON DELETE CASCADE );"
+
+
+psql myDB -c "CREATE TABLE categories ( id_category SERIAL PRIMARY KEY, category TEXT UNIQUE NOT NULL );"
